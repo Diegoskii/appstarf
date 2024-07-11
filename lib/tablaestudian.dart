6dce27e44e86ff74bc71408d685d87ab285532cb
JosuDiaz <159938427+JosuDiaz@users.noreply.github.com>
2024-07-11 08:56:00 -0600

    boton de retroceder

--- lib/tablaestudian.dart
+++ lib/tablaestudian.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'pantprincipaladmin.dart'; // Asegúrate de que este archivo existe y está en la ruta correcta
 
 void main() {
   runApp(RegistroApp());
@@ -55,6 +56,12 @@ class _MyHomePageState extends State<MyHomePage> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      if (index == 0) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => pantallaPrincipalAdmin()),
+        );
+      }
     });
   }
 
